a3876d58ae1cc08ca1cc136099c3ac10d8a05f14
John <jprom254@gmail.com>
2021-03-25 22:36:33 -0400

    Add 8-direction movement
    DaBaby car

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,67 @@
 #include <SFML/Graphics.hpp>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
-int main(){
+bool randBool() {
+    return rand() % 2;
+}
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    window.setFramerateLimit(1);
+int main() {
+    sf::Clock tr;
 
-    while (window.isOpen())
-    {
+    sf::Texture texture;
+    texture.loadFromFile("src/textures/dababycar.jpg");
+
+    sf::RenderWindow window(sf::VideoMode(1200, 900), "SFML window");
+    window.setFramerateLimit(10);
+
+    sf::Sprite car;
+    car.setTexture(texture);
+    car.setScale(.5, .5);
+
+    int speed = 50;
+
+    while (window.isOpen()) {
         // Process events
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+
+        while (window.pollEvent(event)) {
             // Close window: exit
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
         }
         // Clear screen
 
+        if (randBool() == true) {
+            window.clear(sf::Color::Blue);
+        } else {
+            window.clear(sf::Color::Red);
+        }
+
+        float deltaTime = tr.restart().asSeconds();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            car.move(0.f, -speed * deltaTime);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            car.move(0.f, speed * deltaTime);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            car.move(-speed * deltaTime, 0.f);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            car.move(speed * deltaTime, 0.f);
+        }
+
+        window.draw(car);
+
         window.display();
-        
     }
     return EXIT_SUCCESS;
-
-    return 0;
 }
